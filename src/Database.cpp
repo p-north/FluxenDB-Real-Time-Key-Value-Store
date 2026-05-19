0ed1391c5f7a73d458651b1c962e59b572ffa534
Puneet North <puneetnorth170@gmail.com>
2026-05-18 17:09:13 -0700

    add load function implmentation

--- src/Database.cpp
+++ src/Database.cpp
@@ -2,6 +2,7 @@
 
 #include <mutex>
 #include <fstream>
+#include <sstream>
 
 // Singleton pattern -> returns db object instance
 Database &Database::getInstance()
@@ -67,5 +68,48 @@ bool Database::load(const std::string &filename){
     hash_store.clear();
 
     std::string line;
+    // reading from db file
+    while(std::getline(ifs, line)){
+        std::istringstream iss(line);
+        char type;
+        iss >> type;
+        if(type == 'K'){
+            std::string key, value;
+            iss >> key >> value;
+            // add to data structure
+            kv_store[key] = value;
+        }
+        else if(type == 'L'){
+            // get the key
+            std::string key;
+            iss >> key;
+            // get the value
+            std::string item;
+            std::vector<std::string> list;
+            while(iss >> item){
+                list.push_back(item);
+            }
+            list_store[key] = list;
+        }
+        else if(type == 'H'){
+            std::string key;
+            iss >> key;
+            std::unordered_map<std::string, std::string> hash;
+            std::string pair;
+            while(iss >> pair){
+                // split from colon
+                auto pos = pair.find(':');
+                if(pos != std::string::npos){
+                    std::string field = pair.substr(0, pos);
+                    std::string value = pair.substr(pos+1);
+                    hash[field] = value;
+                }
+
+            }
+            hash_store[key] = hash;
+
+        }
+
+    }
 
 }
