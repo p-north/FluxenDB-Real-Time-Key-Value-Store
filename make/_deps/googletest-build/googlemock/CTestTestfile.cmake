5e9ab30494f51a4fbc50585e864050997e7e30f1
Gurshan Singh Aulakh <shan.aulakh29@gmail.com>
2026-05-21 17:01:30 -0700

    Remove build folder from repository

--- make/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shan/Desktop/FluxenDB-Real-Time-Key-Value-Store/make/_deps/googletest-src/googlemock
-# Build directory: /Users/shan/Desktop/FluxenDB-Real-Time-Key-Value-Store/make/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
